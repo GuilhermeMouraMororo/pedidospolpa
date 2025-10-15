ca372f28bb2e3fc82dc37c82bdc3df95b797185e
GuilhermeMouraMororo <guilhermemouramororo@gmail.com>
2025-10-15 14:41:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -876,4 +876,4 @@ def reset_session():
     return jsonify({'success': True})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
